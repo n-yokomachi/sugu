5c9da8e13a5c7cbbc524caa98bb6864f7c101cf9
n-yokomachi <asterism.mihono@gmail.com>
2026-01-02 01:56:20 +0900

    配列・マップ用の組み込み関数を追加

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -50,4 +50,118 @@ var builtins = map[string]*object.Builtin{
 			return &object.String{Value: string(args[0].Type())}
 		},
 	},
+	"len": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Number{Value: float64(len(arg.Value))}
+			case *object.Array:
+				return &object.Number{Value: float64(len(arg.Elements))}
+			case *object.Map:
+				return &object.Number{Value: float64(len(arg.Pairs))}
+			default:
+				return newError("argument to `len` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+			return &object.Array{Elements: newElements}
+		},
+	},
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+			return NULL
+		},
+	},
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				return arr.Elements[length-1]
+			}
+			return NULL
+		},
+	},
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1)
+				copy(newElements, arr.Elements[1:length])
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
+		},
+	},
+	"keys": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.MAP_OBJ {
+				return newError("argument to `keys` must be MAP, got %s", args[0].Type())
+			}
+			mapObj := args[0].(*object.Map)
+			keys := make([]object.Object, 0, len(mapObj.Pairs))
+			for _, pair := range mapObj.Pairs {
+				keys = append(keys, pair.Key)
+			}
+			return &object.Array{Elements: keys}
+		},
+	},
+	"values": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.MAP_OBJ {
+				return newError("argument to `values` must be MAP, got %s", args[0].Type())
+			}
+			mapObj := args[0].(*object.Map)
+			values := make([]object.Object, 0, len(mapObj.Pairs))
+			for _, pair := range mapObj.Pairs {
+				values = append(values, pair.Value)
+			}
+			return &object.Array{Elements: values}
+		},
+	},
 }
