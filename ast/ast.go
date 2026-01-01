cf8b537be5e3621a28b80ec77150f6f53384ce09
n-yokomachi <asterism.mihono@gmail.com>
2026-01-02 01:49:09 +0900

    マップリテラルとキーアクセスを実装

--- ast/ast.go
+++ ast/ast.go
@@ -440,3 +440,23 @@ func (ie *IndexExpression) String() string {
 	out.WriteString("])")
 	return out.String()
 }
+
+// MapLiteral はマップリテラル（{"key": "value"}）
+type MapLiteral struct {
+	Token token.Token // '{' トークン
+	Pairs map[Expression]Expression
+}
+
+func (ml *MapLiteral) expressionNode()      {}
+func (ml *MapLiteral) TokenLiteral() string { return ml.Token.Literal }
+func (ml *MapLiteral) String() string {
+	var out bytes.Buffer
+	pairs := []string{}
+	for key, value := range ml.Pairs {
+		pairs = append(pairs, key.String()+": "+value.String())
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+	return out.String()
+}
